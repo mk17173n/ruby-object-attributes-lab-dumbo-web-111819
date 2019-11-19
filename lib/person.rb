104d112e1ffaebaf098f6769c2cc6e9da1a4af70
Monsur Khan <monsur.khan@gmail.com>
2019-11-19 21:30:24 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+ 
+  def name=(persons_name)
+    @name = persons_name
+  end
+ 
+  def name
+    @name
+  end
+  
+  #Getter
+  def job=(persons_job)
+    @job = persons_job
+  end
+  
+  #Setter
+  def job
+    @job
+  end
+  
+end
