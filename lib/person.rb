46608509f84dfdc98e4067b907fbf8085ba870f6
Monsur Khan <monsur.khan@gmail.com>
2019-11-19 21:28:14 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+ 
+  def name=(persons_name)
+    @name = persons_name
+  end
+ 
+  def name
+    @name
+  end
+  
+  def job=(dogs_breed)
+    @breed = dogs_breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
